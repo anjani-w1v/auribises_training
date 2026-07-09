2d861974b35731fcd49dfdb8eaa4a4b0a63a88a3
anjani-w1v <anjani08yt@gmail.com>
2026-07-09 08:42:31 +0530

    day11

--- session11.py
+++ session11.py
@@ -1,18 +1,51 @@
-'''
-File IO: file input utput stream 
-
 # '''
-# # file = open('session7C.py', 'r')
-# file = open('/Users/anjani/auribisesTraining/a.txt', 'r')
-# lines=file.readlines()
-# file.close()
+# File IO: file input utput stream 
+
+# # '''
+# # # file = open('session7C.py', 'r')
+file = open('/Users/anjani/auribisesTraining/a.txt', 'r')
+# # lines=file.readlines()
+# # file.close()
+# # print(type(lines), len(lines))
+
+# # # for line in lines:
+# # #     print(line)
+
+
+# with open('session11A.py') as file:
+#     lines=file.readlines()
+#     for line in lines:
+#         print(line)
+
+
+
+
+#——————————————————————————
+#——————————————————————————
+
+
+# file = open('Session10C.py', 'r')
+# file = open('/Users/ishant/Downloads/awesome.txt', 'r')
+#print(type(file))            #<class '_io.TextIOWrapper'>
+# data = file.read()
+# print(type(data))
+# print(len(data))
+# print(data)
+
+# lines = file.readlines()         either read(), readlines()
+# file.close() # release memory resources          closing is imprtant
 # print(type(lines), len(lines))
 
-# # for line in lines:
-# #     print(line)
+# # print(lines)
+
+# for line in lines:
+#     print(line)
 
 
-with open('session11A.py') as file:
-    lines=file.readlines()
-    for line in lines:
-        print(line)
+# with open('Session10C.py') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         print(line)
+
+# file.close()
+# print('Program Finished')
--- session11A.py
+++ session11A.py
@@ -1,3 +1,15 @@
+# # file = open('quotes.txt', 'w') # write
+# file = open('quotes.txt', 'a') # append mode
+# quote = input('Enter a Quote: ')
+# file.write(quote+'\n')
+# file.close()
+# print('Quote Saved...')
+
+
+#——————————————————————————
+#——————————————————————————
+
+
 # file = open('quotes.txt', 'w')
 # file = open('quotes.txt', 'a')
 # quote=input('Enter a quote: ')
