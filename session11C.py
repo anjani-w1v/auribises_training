490c3bcb8a3adcaccc9c372c4997fe613da29560
anjani-w1v <anjani08yt@gmail.com>
2026-07-12 23:10:35 +0530

    session 9, 11

--- session11C.py
+++ session11C.py
@@ -1,13 +1,23 @@
+# Pythons Built In Module (JSON)
 import json
-order = {
-    'oid': 1
 
+order = {
+    'oid': 1, 
+    'customer': 'John',
+    'dishes': 'dal, paneer, roti',
+    'amount': 500
 }
 
-import requests
-import json
-api_key=''
-newsapi_url='' 
-response=requests.get(newsapi_url)
-news_json=response.text
-news_dictionary=json.loads(news_json)
+# print(type(order))
+# print(order)
+
+# Dictiory to JSON (String representation of Dictionary)
+json_order = json.dumps(order)         #dump + s string
+
+print(type(json_order))            #<class 'str'>       order dict ta but dumps cnvert it t text i.e string
+print(json_order)         #gives in dict
+
+# JSON to Dictiory (String converted to Dictionary)
+order_dictionary = json.loads(json_order)
+print(type(order_dictionary))        #<class 'dict'>
+print(order_dictionary)         # dict
---
+++ session9C.py
@@ -0,0 +1,64 @@
+# class SongList:
+class CircularDoublyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        print('[CircularDoublyLinkedList] [init] Object Constrcuted', self)
+
+
+    def add(self, element):
+        
+        self.size += 1
+
+        if self.head == None:
+            self.head = element
+            self.tail = element
+        else:
+            self.tail.next_song = element
+            self.head.previous_song = element
+            element.previous_song = self.tail
+            element.next_song = self.head
+            self.tail = element
+
+    def add_in_front(self, element):
+        pass
+
+    def add_in_between(self, element, element1, element2):
+        pass
+
+    def delete_last():
+        pass
+
+    def delete_front():
+        pass
+
+    def delete(element):
+        pass
+
+    def show(self, traverse=True):
+        
+        if traverse == True:
+        
+            song = self.head
+
+            while True:
+                song.show_song()
+                song = song.next_song
+
+                if song == self.head:
+                    break
+        
+        else:
+
+            song = self.tail
+
+            while True:
+                song.show_song()
+                song = song.previous_song
+
+                if song == self.tail:
+                    break
+
+    
